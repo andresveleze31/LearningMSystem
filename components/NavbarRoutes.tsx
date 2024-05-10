7b762bfedbcbb850d48780caec61f7be1d95108b
andresveleze31 <142637448+andresveleze31@users.noreply.github.com>
2024-05-10 09:35:24 -0500

    Addin DB

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,14 +1,37 @@
-"use client"
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
-  return (
-    <div className='flex gap-x-2 ml-auto'>
-        <UserButton />
-      
-    </div>
-  )
-}
+  const pathname = usePathname();
+  const router = useRouter();
 
-export default NavbarRoutes
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Link href={"/"}>
+          <Button size={"sm"} variant={"ghost"}>
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href={"/teacher/courses"}>
+          <Button size={"sm"} variant={"ghost"}>
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default NavbarRoutes;
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/",
+  "/teacher(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
